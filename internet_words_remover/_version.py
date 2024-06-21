e583dbb6b404081dfdfab1ab566c626928f68f14
mrqadeer <mrqadeer1231122@gmail.com>
2024-06-21 21:07:27 +0500

    testing package signature during CI-CD

--- internet_words_remover/_version.py
+++ internet_words_remover/_version.py
@@ -5,8 +5,8 @@ version_json = '''
  "date": "2024-05-02T00:00:00-0000",
  "dirty": false,
  "error": null,
- "full-revisionid": "4b4bb47b82062a17311aef1aac3102381f73b2fe",
- "version": "3.1.1"
+ "full-revisionid": "d39af7d9bec3888ba4036f200bd45f8e8457b53f",
+ "version": "3.1.2"
 }
 '''  # END VERSION_JSON
 
